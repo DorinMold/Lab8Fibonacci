b556697aea1e5f04fe8b9dbe1fb1a4b781d1d9ac
Dorin Moldovan <moldovandorinv@gmail.com>
2020-03-19 22:37:08 +0200

    Primul commiy

---
+++ Tema Lab 8 Fibonacci/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tema_Lab_8_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
